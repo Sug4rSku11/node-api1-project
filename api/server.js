848e4f14c0876eb93e0044c143719286621a967b
Desiree Garcia <57469534+Sug4rSku11@users.noreply.github.com>
2021-11-29 13:59:32 -0700

    PUT and DELETE are now working

--- api/server.js
+++ api/server.js
@@ -54,54 +54,38 @@ server.get('/api/users/:id', async (req, res) => {
     }
 })
 
-//DELETE -- specific id Delete /api/users/:id
+//DELETE -- specific id Delete /api/users/:id ***Works***
 server.delete('/api/users/:id', async (req, res) => {
-    const { id } = req.params
     try{
-        const user = await user.delete(id)
-        if(!user){
-            res.status(404).json({
-                message: "The user with the specified ID does not exist"
-            })
+        const possibleUser = await User.findById(req.params.id)
+        if(!possibleUser) {
+            res.status(404).json ({ message: "The user with the specified ID does not exist"})
+        }else {
+            const deletedUser = await User.remove(req.params.id)
+            res.status(200).json(deletedUser)
         }
     }catch (err){
-        res.status(500).json ({
-            message: "The user could not be removed"
-        })
+        res.status(500).json({ message: "The user could not be removed", err: err.message})
     }
-})
+        })
+    
+
 //UPDATE-- specific user Put /api/users/:id
 server.put('/api/users/:id', async (req, res) => {
-    const { id } = req.params
-    const { body } = req
     try{
-        const updated = await User.update(id, body)
-        if(!updated){
-            res.status(404).json ({
-                message: "The user with the specified ID does not exist"
-            })
+        const possibleUser = await User.findById(req.params.id)
+        if(!possibleUser) {
+            res.status(404).json({ message: "The user with the specified ID does not exist"})
         }else{
-            res.json(updated)
+            if (!req.body.name || !req.body.bio){
+                res.status(400).json({ message: "Please provide name and bio for the user"})   
+            }else{
+                const updatedUser = await User.update(req.params.id , req.body)
+                res.status(200).json(updatedUser)
+            }
         }
-        if(!updated){
-            res.status(404).json ({
-                message: "The user with the specified ID does not exist"
-            })
-        }else{
-            res.json(updated)
-        }
-        if(!req.body.name || !req.body.bio){
-            res.status(400).json({
-                message: "Please provide name and bio for the user"
-            })
-        }else{
-            res.json(updated)
-        }
-
-    }catch (err) {
-        res.status(500).json({
-            message: "The user information could not be modified"
-        })
+    }catch(err){
+        res.status(500).json ({ message: "The user information could not be modified", err: err.message})
     }
 })
 
